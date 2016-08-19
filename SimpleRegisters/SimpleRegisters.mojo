02a8396886785b69b44d37ed49856ab02a6d45e8
Oka Kurniawan <kurniawano@ieee.org>
2016-08-19 16:40:15 +0800

    add simple register example.

---
+++ SimpleRegisters/SimpleRegisters.mojo
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project name="SimpleRegisters" board="Mojo V3" language="Lucid">
+  <files>
+    <src>register.luc</src>
+    <src top="true">mojo_top.luc</src>
+    <ucf lib="true">io_shield.ucf</ucf>
+    <ucf lib="true">mojo.ucf</ucf>
+    <component>reset_conditioner.luc</component>
+    <component>pipeline.luc</component>
+    <component>button_conditioner.luc</component>
+    <component>edge_detector.luc</component>
+  </files>
+</project>
---
+++ SimpleRegisters/work/project.tcl
@@ -0,0 +1,22 @@
+set projDir "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/planAhead"
+set projName "SimpleRegisters"
+set topName top
+set device xc6slx9-2tqg144
+if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
+create_project $projName "$projDir/$projName" -part $device
+set_property design_mode RTL [get_filesets sources_1]
+set verilogSources [list "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/verilog/mojo_top_0.v" "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/verilog/reset_conditioner_1.v" "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/verilog/button_conditioner_2.v" "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/verilog/edge_detector_3.v" "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/verilog/register_4.v" "C:/Users/TABLET0006/Box Sync/SUTD/ComputationStructures/Mojo/Mojo-Examples/SimpleRegisters/work/verilog/pipeline_5.v"]
+import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
+set ucfSources [list  "C:/Program\ Files/Mojo\ IDE/library/components/io_shield.ucf" "C:/Program\ Files/Mojo\ IDE/library/components/mojo.ucf"]
+import_files -fileset [get_filesets constrs_1] -force -norecurse $ucfSources
+set_property -name {steps.bitgen.args.More Options} -value {-g Binary:Yes -g Compress} -objects [get_runs impl_1]
+set_property steps.map.args.mt on [get_runs impl_1]
+set_property steps.map.args.pr b [get_runs impl_1]
+set_property steps.par.args.mt on [get_runs impl_1]
+update_compile_order -fileset sources_1
+launch_runs -runs synth_1
+wait_on_run synth_1
+launch_runs -runs impl_1
+wait_on_run impl_1
+launch_runs impl_1 -to_step Bitgen
+wait_on_run impl_1
